66cd44cbcb4756fb12433491ea71248df44a4a8e
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-10 17:06:56 +0700

    add: yeah

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,154 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import { useState, useEffect, useRef } from "react";
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { PhotoAlbum } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css"
 
-const inter = Inter({ subsets: ['latin'] })
+//import images
+import avatar from "../images/avatar.png";
+import derry from "../images/1.jpg"
+import virginia from "../images/virginia.jpeg"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [step, setStep] = useState(0);
+    const [index, setIndex] = useState(-1);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    // Map
+    const mapContainer = useRef(null)
+    const mapContainer2 = useRef(null)
+    const map = useRef(null);
+    const map2 = useRef(null);
+    const [lng, setLng] = useState(108.18996767210655);
+    const [lat, setLat] = useState(-6.760812337288243);
+    const [zoom, setZoom] = useState(15);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    useEffect(() => {
+        loadMap();
+        // loadMap2();
+        if (!map.current) return;
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4))
+            setLat(map.current.getCenter().lat.toFixed(4))
+            setZoom(map.current.getZoom().toFixed(2))
+        })
+    })
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const loadMap = async () => {
+        mapboxgl.accessToken = "pk.eyJ1IjoiaGF0c3VzaGlyb3l1a2kiLCJhIjoiY2xnMGt4MXNwMHR0NDNqcDlmb3NsdjM3ZSJ9.UEWrPDqMO_uH6ulxOOCkyw";
+        if (map.current) return;
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/outdoors-v12',
+            center: [lng, lat],
+            zoom: zoom
+        })
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    //   const loadMap2 = async () => {
+    //     mapboxgl.accessToken = "pk.eyJ1IjoiaGF0c3VzaGlyb3l1a2kiLCJhIjoiY2xnMGt4MXNwMHR0NDNqcDlmb3NsdjM3ZSJ9.UEWrPDqMO_uH6ulxOOCkyw";
+    //     if (map2.current) return;
+
+    //     map2.current = new mapboxgl.Map({
+    //       container: mapContainer2.current,
+    //       style: 'mapbox://styles/mapbox/outdoors-v12',
+    //       center: [lng, lat],
+    //       zoom: zoom
+    //     })
+    //   }
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <div className="hero min-h-screen shadow-lg">
+                <div className="hero-overlay bg-opacity-50">
+                    <div className='overlay opacity-20'>
+                        <Image src={"/images/bg.webp"} layout="fill" objectFit="cover" />
+                    </div>
+                </div>
+                <div className="card w-72">
+                    <figure>
+                        <Image src={avatar} className="mask mask-squircle scale-75" width={500} height={500} />
+                    </figure>
+                    <h1 className="text-center text-3xl">Derry & Virginia</h1>
+                    <div className="card-body">
+                        <p className="text-center">Kepada Yth.</p>
+                        <p className="text-center">Mohon maaf apabila terdapat kesalahan nama/gelar</p>
+                    </div>
+                </div>
+            </div>
+            <section id="uhhh" className="hero-overlay shadow-lg">
+                <div className="card-body flex flex-col">
+                    <h1 className="text-center text-3xl pb-5 font-['Dancing_Script']">We found love</h1>
+                    <p className="text-center flex px-10">
+                        “Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.”</p>
+                    <p className="text-center pt-5">QS. Ar-Rum ayat 21</p>
+                </div>
+            </section>
+            <section className="shadow-lg pb-5">
+                <h1 className="text-center text-3xl">Kedua Mempelai</h1>
+                <p className="text-center text-m pt-3 pb-3">Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud menyelenggarakan acara pernikahan putra-putri kami</p>
+                <div class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10 mx-auto">
+                    <div className="card w-96 bg-white shadow-2xl">
+                        <figure>
+                            <Image src={derry} className="mask mask-squircle scale-75" width={500} height={500} alt="derry_photo" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Derry Muliawan, S.T.</h2>
+                            <p>Putra dari : Bapak H. Otong Junaedi (Alm) dan Ibu Hj. Susilawati (Alm).</p>
+                        </div>
+                    </div>
+                    <div className="card w-96 bg-white">
+                        <figure>
+                            <Image src={virginia} className="mask mask-squircle scale-75" width={500} height={500} alt="virginia_photo" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Virginia Sari, S.Pd.</h2>
+                            <p>Putri dari : Bapak H. Mulyadi dan Ibu Hj. Siti Nurhayati.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="gallery">
+                <div className='hero-content flex flex-col mx-auto' id='gallery'>
+                    <h1 className='text-5xl text-left'>Gallery</h1>
+                </div>
+                <div className="hero-content flex flex-col mx-auto" id="gallery">
+                    asdsad
+                </div>
+            </section>
+            <section className='flex justify-center animate-fadeIn' id='date'>
+                <div className='hero-content flex-col lg:flex-row justify-between w-full'>
+                    <Image src={"/images/shape-1.png"} width={100} height={100} />
+                    <div className='mx-auto'>
+                        <div className='grid md:grid'>
+                            <div className='flex flex-col'>
+                                <h1 className='text-4xl font-bold pt-5 mb-2 text-white drop-shadow-md'>Minggu</h1>
+                                <p className='text-2xl text-white drop-shadow-md'>18 Maret 2024</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='w-[400px] h-[200px] shadow-xl' id='map'>
+                            <div ref={mapContainer} className="map-container" />
+                        </div>
+                        <div className='pt-5 text-blue-600 text-center'>
+                            <a href="https://www.google.com" target="_blank">
+                                <button className="btn btn-wide">Lihat di Google maps</button>
+                            </a>
+                        </div>
+                    </div>
+                    <Image src={"/images/shape-2.png"} width={100} height={100} />
+                </div>
+            </section>
+            <section>
+                asdsad
+            </section>
+        </>
+    );
 }
