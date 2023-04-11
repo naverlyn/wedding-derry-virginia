dca058332239b10c0ed7a282601af6fdbd861cdc
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-11 14:13:02 +0700

    changed: box and gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,15 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { PhotoAlbum } from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css"
+import dynamic from "next/dynamic";
+import images from "../../src/images/photo.js";
 
 //import images
 import avatar from "../images/avatar.png";
 import derry from "../images/1.jpg"
 import virginia from "../images/virginia.jpeg"
+import flower1 from "../images/shape-1.png"
+import flower2 from "../images/shape-2.png"
 
 export default function Home() {
     const [step, setStep] = useState(0);
@@ -93,23 +97,31 @@ export default function Home() {
             <section className="shadow-lg pb-5">
                 <h1 className="text-center text-3xl">Kedua Mempelai</h1>
                 <p className="text-center text-m pt-3 pb-3">Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud menyelenggarakan acara pernikahan putra-putri kami</p>
-                <div class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10 mx-auto">
-                    <div className="card w-96 bg-white shadow-2xl">
-                        <figure>
-                            <Image src={derry} className="mask mask-squircle scale-75" width={500} height={500} alt="derry_photo" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Derry Muliawan, S.T.</h2>
-                            <p>Putra dari : Bapak H. Otong Junaedi (Alm) dan Ibu Hj. Susilawati (Alm).</p>
+                <div className="container mx-auto content-center">
+                    <div className="grid grid-cols-2 justify-center gap-1">
+                        <div className="m-4 mr-0 my-20 max-w-md rounded-lg bg-blue-100 py-4 px-8 shadow-lg justify-center md:gap-4">
+                            <div className="-mt-16 flex justify-center md:justify-end">
+                                <Image src={derry} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
+                            </div>
+                            <div>
+                                <p className="mt-2 text-gray-600 text-xl">Derry Muliawan, S.T.</p>
+                                <p className="mt-2 text-gray-600 text-m">Putra dari : Bapak H. Otong Junaedi (Alm) dan Ibu Hj. Susilawati (Alm).</p>
+                            </div>
+                            <div className="mt-4 flex justify-end">
+                                <a href="https://www.facebook.com/derry.mulyawan" target="_blank"><svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 12.05C19.9813 10.5255 19.5273 9.03809 18.6915 7.76295C17.8557 6.48781 16.673 5.47804 15.2826 4.85257C13.8921 4.2271 12.3519 4.01198 10.8433 4.23253C9.33473 4.45309 7.92057 5.10013 6.7674 6.09748C5.61422 7.09482 4.77005 8.40092 4.3343 9.86195C3.89856 11.323 3.88938 12.8781 4.30786 14.3442C4.72634 15.8103 5.55504 17.1262 6.69637 18.1371C7.83769 19.148 9.24412 19.8117 10.75 20.05V14.38H8.75001V12.05H10.75V10.28C10.7037 9.86846 10.7483 9.45175 10.8807 9.05931C11.0131 8.66687 11.23 8.30827 11.5161 8.00882C11.8022 7.70936 12.1505 7.47635 12.5365 7.32624C12.9225 7.17612 13.3368 7.11255 13.75 7.14003C14.3498 7.14824 14.9482 7.20173 15.54 7.30003V9.30003H14.54C14.3676 9.27828 14.1924 9.29556 14.0276 9.35059C13.8627 9.40562 13.7123 9.49699 13.5875 9.61795C13.4627 9.73891 13.3667 9.88637 13.3066 10.0494C13.2464 10.2125 13.2237 10.387 13.24 10.56V12.07H15.46L15.1 14.4H13.25V20C15.1399 19.7011 16.8601 18.7347 18.0985 17.2761C19.3369 15.8175 20.0115 13.9634 20 12.05Z" fill="#000000"></path> </g></svg></a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="card w-96 bg-white">
-                        <figure>
-                            <Image src={virginia} className="mask mask-squircle scale-75" width={500} height={500} alt="virginia_photo" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Virginia Sari, S.Pd.</h2>
-                            <p>Putri dari : Bapak H. Mulyadi dan Ibu Hj. Siti Nurhayati.</p>
+                        <div className="ml-0 my-20 max-w-md rounded-lg bg-pink-100 py-4 px-8 shadow-lg justify-center md:gap-4">
+                            <div className="-mt-16 flex justify-center md:justify-end ">
+                                <Image src={virginia} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
+                            </div>
+                            <div>
+                                <p className="mt-2 text-gray-600 text-xl">Virginia Sari, S.Pd.</p>
+                                <p className="mt-2 text-gray-600 text-m">Putri dari : Bapak H. Mulyadi dan Ibu Hj. Siti Nurhayati.</p>
+                            </div>
+                            <div className="mt-4 flex justify-end">
+                                <a href="https://www.facebook.com/derry.mulyawan" target="_blank"><svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 12.05C19.9813 10.5255 19.5273 9.03809 18.6915 7.76295C17.8557 6.48781 16.673 5.47804 15.2826 4.85257C13.8921 4.2271 12.3519 4.01198 10.8433 4.23253C9.33473 4.45309 7.92057 5.10013 6.7674 6.09748C5.61422 7.09482 4.77005 8.40092 4.3343 9.86195C3.89856 11.323 3.88938 12.8781 4.30786 14.3442C4.72634 15.8103 5.55504 17.1262 6.69637 18.1371C7.83769 19.148 9.24412 19.8117 10.75 20.05V14.38H8.75001V12.05H10.75V10.28C10.7037 9.86846 10.7483 9.45175 10.8807 9.05931C11.0131 8.66687 11.23 8.30827 11.5161 8.00882C11.8022 7.70936 12.1505 7.47635 12.5365 7.32624C12.9225 7.17612 13.3368 7.11255 13.75 7.14003C14.3498 7.14824 14.9482 7.20173 15.54 7.30003V9.30003H14.54C14.3676 9.27828 14.1924 9.29556 14.0276 9.35059C13.8627 9.40562 13.7123 9.49699 13.5875 9.61795C13.4627 9.73891 13.3667 9.88637 13.3066 10.0494C13.2464 10.2125 13.2237 10.387 13.24 10.56V12.07H15.46L15.1 14.4H13.25V20C15.1399 19.7011 16.8601 18.7347 18.0985 17.2761C19.3369 15.8175 20.0115 13.9634 20 12.05Z" fill="#000000"></path> </g></svg></a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -119,12 +131,17 @@ export default function Home() {
                     <h1 className='text-5xl text-left'>Gallery</h1>
                 </div>
                 <div className="hero-content flex flex-col mx-auto" id="gallery">
-                    asdsad
+                    <PhotoAlbum
+                        layout="grid"
+                        photos={images}
+                        targetRowHeight={150}
+                        onClick={({ index }) => setIndex(index)}
+                    />
                 </div>
             </section>
             <section className='flex justify-center animate-fadeIn' id='date'>
                 <div className='hero-content flex-col lg:flex-row justify-between w-full'>
-                    <Image src={"/images/shape-1.png"} width={100} height={100} />
+                    <Image src={flower1} width={100} height={100} />
                     <div className='mx-auto'>
                         <div className='grid md:grid'>
                             <div className='flex flex-col'>
@@ -143,7 +160,7 @@ export default function Home() {
                             </a>
                         </div>
                     </div>
-                    <Image src={"/images/shape-2.png"} width={100} height={100} />
+                    <Image src={flower2} width={100} height={100} />
                 </div>
             </section>
             <section>
