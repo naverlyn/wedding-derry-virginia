90c7ba78c2a297299b315c41082fa942eccd0d8d
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-12 12:27:48 +0700

    add: slides

---
+++ src/images/slides.js
@@ -0,0 +1,15 @@
+import photos from "./photos";
+
+export const slides = photos.map(({ src, key, width, height, images }) => ({
+  src,
+  key,
+  width,
+  height,
+  srcSet: images?.map((image) => ({
+    src: image.src,
+    width: image.width,
+    height: image.height
+  }))
+}));
+
+export default slides;
