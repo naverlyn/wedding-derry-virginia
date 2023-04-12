d7ac419f4363a2e288ced1d722a85db1c3c8983f
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-12 13:41:17 +0700

    add: animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,15 +15,57 @@ module.exports = {
         Sacramento: ["Sacramento", "cursive"],
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'hero':  "url('https://unsplash.com/photos/2a1GNZZaVHk')",
+        'background': "url('../images/bg.jpg')",
+        'background2': "url('../images/bg2.jpg')",
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        tada: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '10%, 30%, 50%, 70%, 90%': { transform: 'scale(0.9)' },
+          '20%, 40%, 60%, 80%': { transform: 'scale(1.1)' },
+        },
+        jello: {
+          'from, 11.1%,to': {
+            transform: 'translate3d(0, 0, 0)',
+          },
+
+          '22.2%': {
+            transform: 'skewX(-12.5deg) skewY(-12.5deg)',
+          },
+
+          '33.3%': {
+            transform: 'skewX(6.25deg) skewY(6.25deg)',
+          },
+
+          '44.4%': {
+            transform: 'skewX(-3.125deg) skewY(-3.125deg)',
+          },
+
+          '55.5%': {
+            transform: 'skewX(1.5625deg) skewY(1.5625deg)',
+          },
+
+          '66.6%': {
+            transform: 'skewX(-0.78125deg) skewY(-0.78125deg)',
+          },
+
+          '77.7%': {
+            transform: 'skewX(0.390625deg) skewY(0.390625deg)',
+          },
+
+          '88.8%': {
+            transform: 'skewX(-0.1953125deg) skewY(-0.1953125deg)',
+          },
+        },
       },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-        'fade-in': 'fadeIn 1s ease-in-out',
-        'swipe-right': 'swipeRight 1s ease-in-out',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        tada: 'tada 10s ease-in-out',
+        jello: 'jello 1s',
       },
     },
   },
