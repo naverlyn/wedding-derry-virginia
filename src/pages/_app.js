74307a4c76b8c3cb07cd0c7be1cb1e9548c2fceb
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-12 15:17:44 +0700

    add: AOS

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 1000,
+      easing: 'ease-in-out',
+      delay: 200,
+      disable: 'mobile',
+      mirror: false,
+      anchorPlacement: 'top-bottom'
+    })
+  })
   return <Component {...pageProps} />
 }
