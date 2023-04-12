5964db77cdd2e9600c3684b0c5a5b55cb565a600
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-12 11:47:30 +0700

    fix: load font

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import images from "../../src/images/photo";
 import avatar from "../images/avatar.png";
 import derry from "../images/1.jpg"
 import virginia from "../images/virginia.jpeg"
+import bg from "../images/bg.webp"
 import flower1 from "../images/shape-1.png"
 import flower2 from "../images/shape-2.png"
 
@@ -71,16 +72,16 @@ export default function Home() {
             <div className="hero min-h-screen shadow-lg">
                 <div className="hero-overlay bg-opacity-90">
                     <div className='opacity-70'>
-                        <Image src={"/images/bg.webp"} layout="fill" objectFit="cover" className="bg-cover bg-center" />
+                        <Image src={bg} layout="fill" objectFit="cover" className="bg-cover bg-center" />
                     </div>
                 </div>
                 <div className="card w-72 rounded-xl backdrop-blur border-2 animate-swipe-right">
                     <figure>
                         <Image src={avatar} className="mask mask-squircle scale-75" width={500} height={500} />
                     </figure>
-                    <h1 className="text-center text-3xl text-white">Derry & Virginia</h1>
+                    <h1 className="text-center text-3xl text-white font-Allura">Derry & Virginia</h1>
                     <div className="card-body">
-                        <p className="text-center text-white">Kepada Yth.</p>
+                        <p className="text-center text-white font-Dancing">Kepada Yth.</p>
                         <p className="text-center text-white">Mohon maaf apabila terdapat kesalahan nama/gelar</p>
                     </div>
                 </div>
@@ -97,7 +98,7 @@ export default function Home() {
                 <p className="text-center text-m pt-3 pb-3">Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud menyelenggarakan acara pernikahan putra-putri kami</p>
                 <div className="container mx-auto content-center">
                     <div className="grid grid-cols-2 justify-center gap-4">
-                        <div className="m-4 mr-0 my-20 max-w-full rounded-lg bg-blue-100 py-4 px-8 shadow-lg justify-center md:gap-4">
+                        <div className="m-4 mr-0 my-20 max-w-full rounded-lg bg-blue-100 py-4 px-4 shadow-lg justify-center">
                             <div className="-mt-16 flex justify-center md:justify-end">
                                 <Image src={derry} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
                             </div>
@@ -109,7 +110,7 @@ export default function Home() {
                                 <a href="https://www.facebook.com/derry.mulyawan" target="_blank"><svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 12.05C19.9813 10.5255 19.5273 9.03809 18.6915 7.76295C17.8557 6.48781 16.673 5.47804 15.2826 4.85257C13.8921 4.2271 12.3519 4.01198 10.8433 4.23253C9.33473 4.45309 7.92057 5.10013 6.7674 6.09748C5.61422 7.09482 4.77005 8.40092 4.3343 9.86195C3.89856 11.323 3.88938 12.8781 4.30786 14.3442C4.72634 15.8103 5.55504 17.1262 6.69637 18.1371C7.83769 19.148 9.24412 19.8117 10.75 20.05V14.38H8.75001V12.05H10.75V10.28C10.7037 9.86846 10.7483 9.45175 10.8807 9.05931C11.0131 8.66687 11.23 8.30827 11.5161 8.00882C11.8022 7.70936 12.1505 7.47635 12.5365 7.32624C12.9225 7.17612 13.3368 7.11255 13.75 7.14003C14.3498 7.14824 14.9482 7.20173 15.54 7.30003V9.30003H14.54C14.3676 9.27828 14.1924 9.29556 14.0276 9.35059C13.8627 9.40562 13.7123 9.49699 13.5875 9.61795C13.4627 9.73891 13.3667 9.88637 13.3066 10.0494C13.2464 10.2125 13.2237 10.387 13.24 10.56V12.07H15.46L15.1 14.4H13.25V20C15.1399 19.7011 16.8601 18.7347 18.0985 17.2761C19.3369 15.8175 20.0115 13.9634 20 12.05Z" fill="#000000"></path> </g></svg></a>
                             </div>
                         </div>
-                        <div className="ml-0 my-20 max-w-full rounded-lg bg-pink-100 py-4 px-8 shadow-lg justify-center md:gap-4">
+                        <div className="ml-0 my-20 max-w-full rounded-lg bg-pink-100 py-4 px-4 shadow-lg justify-center">
                             <div className="-mt-16 flex justify-center md:justify-end ">
                                 <Image src={virginia} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
                             </div>
