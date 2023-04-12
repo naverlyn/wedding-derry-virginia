6f08e9797b12cccdac20bc92e2504b143fefc512
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-12 12:27:41 +0700

    add: photo collection

---
+++ src/images/photos.js
@@ -0,0 +1,452 @@
+const breakpoints = [4320, 2160, 1080, 640, 384, 256, 128];
+
+const unsplashLink = (id) =>
+  `https://imgur.com/${id}`;
+
+const unsplashPhotos = [
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+  {
+    id: "EjQhyBG.jpg",
+    width: 286,
+    height: 256
+  },
+];
+
+const photos = unsplashPhotos.map((photo, index) => {
+  const width = photo.width * 4;
+  const height = photo.height * 4;
+  return {
+    src: unsplashLink(photo.id, width, height),
+    key: `${index}`,
+    width,
+    height,
+    images: breakpoints.map((breakpoint) => {
+      const breakpointHeight = Math.round((height / width) * breakpoint);
+      return {
+        src: unsplashLink(photo.id, breakpoint, breakpointHeight),
+        width: breakpoint,
+        height: breakpointHeight
+      };
+    })
+  };
+});
+
+export default photos;
