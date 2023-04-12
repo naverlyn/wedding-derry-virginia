18203caece7f057748de81e916a27fba6a6e2d93
Aula Nur Rizal Ardiyantoro <rizalardiyantoro123@gmail.com>
2023-04-12 15:18:15 +0700

    add: using AOS

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,18 +69,18 @@ export default function Home() {
             </Head>
             <div className="hero min-h-screen shadow-lg">
                 <div className="hero-overlay opacity-90">
-                    <div className='opacity-30'>
+                    <div className='opacity-100'>
                         <div className="bg-scroll bg-background bg-cover bg-center h-screen"></div>
                     </div>
                 </div>
-                <div className="card w-80 rounded-xl backdrop-blur-lg border-2 animate-jello">
+                <div className="card w-80 rounded-xl backdrop-blur-lg border-2" data-aos="fade-left" >
                     <figure>
                         <Image src={avatar} className="mask mask-squircle scale-75" width={500} height={500} />
                     </figure>
-                    <h1 className="text-center text-5xl text-white font-Dancing">Derry & Virginia</h1>
+                    <h1 className="text-center text-5xl font-Dancing">Derry & Virginia</h1>
                     <div className="card-body">
-                        <p className="text-center text-white font-Dancing text-3xl">Kepada Yth.</p>
-                        <p className="text-center text-white font-Dancing text-2xl">Mohon maaf apabila terdapat kesalahan nama/gelar</p>
+                        <p className="text-center font-Dancing text-3xl">Kepada Yth.</p>
+                        <p className="text-center  font-Dancing text-xl">Mohon maaf apabila terdapat kesalahan nama / gelar</p>
                     </div>
                 </div>
             </div>
@@ -91,12 +91,12 @@ export default function Home() {
                             <div className="bg-scroll bg-background2 bg-cover bg-center h-screen"></div>
                         </div>
                     </div>
-                    <div className="flex h-screen">
+                    <div className="flex h-full">
                         <div className="m-auto">
-                            <div className="card max-w-md pt-5 rounded-xl backdrop-blur border-2">
-                                <p className="text-center flex px-10 font-Dancing text-4xl text-black drop-shadow-sm">
+                            <div className="card max-w-xl pt-5 rounded-xl backdrop-blur-md border-2 border-white" data-aos="fade-right">
+                                <p className="text-center flex px-10 font-Dancing text-4xl drop-shadow-sm">
                                     “Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.”</p>
-                                <p className="text-center pt-5 text-xl font-Dancing pb-3 text-white shadow-lg">QS. Ar-Rum ayat 21</p>
+                                <p className="text-center pt-5 text-xl font-Dancing pb-3 shadow-lg">QS. Ar-Rum ayat 21</p>
                             </div>
                         </div>
                     </div>
@@ -104,27 +104,33 @@ export default function Home() {
             </section>
             <section className="shadow-lg" id="mempelai">
                 <div className="bg-scroll bg-background2 bg-cover bg-center h-screen">
-                    <h1 className="text-center text-5xl font-Dancing pt-5">Kedua Mempelai</h1>
-                    <p className="text-center text-m pt-3 pb-3 text-3xl font-Dancing">Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud menyelenggarakan acara pernikahan putra-putri kami</p>
-                    <div className="flex">
+                    <div className="flex h-full">
                         <div className="m-auto">
-                            <div className="grid grid-cols-2 justify-center gap-4">
-                                <div className="m-4 mr-0 my-20 max-w-full rounded-lg bg-blue-100 py-4 px-4 shadow-lg justify-center">
-                                    <div className="-mt-16 flex justify-center md:justify-end">
-                                        <Image src={derry} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
-                                    </div>
-                                    <div>
-                                        <p className="mt-2 text-gray-600 text-3xl font-Dancing">Derry Muliawan, S.T.</p>
-                                        <p className="mt-2 text-gray-600 text-2xl font-Sacramento">Putra dari : Bapak H. Otong Junaedi (Alm) dan Ibu Hj. Susilawati (Alm).</p>
-                                    </div>
-                                </div>
-                                <div className="ml-0 my-20 max-w-full rounded-lg bg-pink-100 py-4 px-4 shadow-lg justify-center">
-                                    <div className="-mt-16 flex justify-center md:justify-end ">
-                                        <Image src={virginia} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
-                                    </div>
-                                    <div>
-                                        <p className="mt-2 text-gray-600 text-3xl font-Dancing">Virginia Sari, S.Pd.</p>
-                                        <p className="mt-2 text-gray-600 text-2xl font-Dancing">Putri dari : Bapak H. Mulyadi dan Ibu Hj. Siti Nurhayati.</p>
+                            <div className="card max-w-3xl pt-5 rounded-xl backdrop-blur border-2 border-white" data-aos="fade-up-left">
+                                <h1 className="text-center text-5xl font-Dancing pt-5">Kedua Mempelai</h1>
+                                <p className="text-center text-m pt-3 pb-3 text-3xl font-Dancing">Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud menyelenggarakan acara pernikahan putra-putri kami</p>
+                                <div className="flex min-h-full">
+                                    <div className="m-auto">
+                                        <div className="grid grid-cols-2 justify-center gap-2">
+                                            <div className="m-4 mr-0 my-20 max-w-full rounded-lg bg-blue-100 py-4 px-4 shadow-lg justify-center">
+                                                <div className="-mt-16 flex justify-center md:justify-end">
+                                                    <Image src={derry} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
+                                                </div>
+                                                <div>
+                                                    <p className="mt-2 text-gray-600 text-3xl font-Dancing font-bold">Derry Muliawan, S.T.</p>
+                                                    <p className="mt-2 text-gray-600 text-2xl font-Satisfy">Putra dari : Bapak H. Otong Junaedi (Alm) dan Ibu Hj. Susilawati (Alm).</p>
+                                                </div>
+                                            </div>
+                                            <div className="ml-0 my-20 max-w-full rounded-lg bg-pink-100 py-4 px-4 shadow-lg justify-center">
+                                                <div className="-mt-16 flex justify-center md:justify-end ">
+                                                    <Image src={virginia} width={500} height={500} className="h-32 w-32 rounded-full border-2 border-indigo-500 object-cover" />
+                                                </div>
+                                                <div>
+                                                    <p className="mt-2 text-gray-600 text-3xl font-Dancing font-bold">Virginia Sari, S.Pd.</p>
+                                                    <p className="mt-2 text-gray-600 text-2xl font-Satisfy">Putri dari : Bapak H. Mulyadi dan Ibu Hj. Siti Nurhayati.</p>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -132,86 +138,40 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-            <section className="shadow-lg pb-5 pt-10 font-Dancing" id="date">
-                <h1 className="text-center text-3xl">Yang akan digelar pada Tanggal:</h1>
-                <p className="text-3xl font-bold pt-5 drop-shadow-md text-center">Akad: Sabtu, 06 Mei 2023 Pukul 08:00</p>
-                <p className="text-3xl font-bold pt-5 drop-shadow-md text-center">Resepsi: Sabtu, 06 Mei 2023 pukul 10.00 WIB</p>
-                <p className="text-2xl font-bold justify-center pt-10 text-center">Lokasi: Lingkungan Pesantren Darul Falah RT.01/05 Kel.Cijati Kec. Majalengka</p>
-                <div className="container mx-auto content-center">
-                    <div className="grid grid-rows-2 justify-center">
-                        <div className='w-[400px] h-[200px] shadow-xl' id='map'>
-                            <div ref={mapContainer} className="map-container" />
-                        </div>
-                        <div className='pt-5 text-blue-600 text-center'>
-                            <a href="https://goo.gl/maps/w8GQ7efPsdtP3EY89" target="_blank">
-                                <button className="btn btn-wide font-sans">Lihat di Google maps</button>
-                            </a>
+            <section className="shadow-lg font-Dancing" id="date">
+                <div className="bg-scroll bg-background2 bg-cover bg-center h-screen">
+                    <div className="flex h-full">
+                        <div className="m-auto">
+                            <div className="card max-w-3xl pt-5 rounded-xl backdrop-blur border-2 border-white" data-aos="zoom-in">
+                                <h1 className="text-center text-3xl">Yang akan digelar pada Tanggal:</h1>
+                                <p className="text-3xl font-bold pt-5 drop-shadow-md text-center">Akad: Sabtu, 06 Mei 2023 Pukul 08:00</p>
+                                <p className="text-3xl font-bold pt-5 drop-shadow-md text-center">Resepsi: Sabtu, 06 Mei 2023 pukul 10.00 WIB</p>
+                                <p className="text-2xl font-bold justify-center pt-10 pb-10 text-center">Lokasi: Lingkungan Pesantren Darul Falah RT.01/05 Kel.Cijati Kec. Majalengka</p>
+                                <div className="container mx-auto content-center">
+                                    <div className="grid grid-rows-2 justify-center">
+                                        <div className='w-[400px] h-[200px] shadow-xl' id='map'>
+                                            <div ref={mapContainer} className="map-container" />
+                                        </div>
+                                        <div className='pt-20 text-blue-600 text-center'>
+                                            <a href="https://goo.gl/maps/w8GQ7efPsdtP3EY89" target="_blank">
+                                                <button className="btn btn-wide font-sans">Lihat di Google maps</button>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </section>
-            {/* <section className='max-w-full pt-10' id='date'>
-                <div className="container justify-center">
-                    <div className='hero-content content-center'>
-                        <Image src={flower1} width={100} height={100} />
-                        <div className='mx-auto'>
-                            <p className="text-center ">Yang akan Digelar pada tanggal:</p>
-                            <div className='grid md:grid'>
-                                <div className='flex flex-col md:text-sm'>
-                                    <p className="text-3xl font-bold pt-5 drop-shadow-md">Akad: Sabtu, 06 Mei 2023 Pukul 08:00</p>
-                                    <p className="text-3xl font-bold pt-5 drop-shadow-md">Resepsi: Sabtu, 06 Mei 2023 pukul 10.00 WIB</p>
-                                    <p className="text-2xl font-bold justify-center pt-10">Lokasi: Lingkungan Pesantren Darul Falah RT.01/05 Kel.Cijati Kec. Majalengka</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='w-[400px] h-[200px] shadow-xl' id='map'>
-                                <div ref={mapContainer} className="map-container" />
-                            </div>
-                            <div className='pt-5 text-blue-600 text-center'>
-                                <a href="https://goo.gl/maps/w8GQ7efPsdtP3EY89" target="_blank">
-                                    <button className="btn btn-wide">Lihat di Google maps</button>
-                                </a>
-                            </div>
-                        </div>
-                        <Image src={flower2} width={100} height={100} className="justify-end" />
-                    </div>
-                    <div class="container mx-auto pt-10">
-                        <div className="flex gap-5 justify-center">
-                            <div>
-                                <span className="countdown font-mono text-4xl">
-                                    <span style={{ "--value": 15 }}></span>
-                                </span>
-                                days
-                            </div>
-                            <div>
-                                <span className="countdown font-mono text-4xl">
-                                    <span style={{ "--value": 10 }}></span>
-                                </span>
-                                hours
-                            </div>
-                            <div>
-                                <span className="countdown font-mono text-4xl">
-                                    <span style={{ "--value": 24 }}></span>
-                                </span>
-                                min
-                            </div>
-                            <div>
-                                <span className="countdown font-mono text-4xl">
-                                    <span style={{ "--value": 44 }}></span>
-                                </span>
-                                sec
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
+
             <section id="gallery" className="shadow-md justify-center font-Dancing">
                 <div className='hero-content flex flex-col mx-auto' id='gallery'>
                     <h1 className='text-5xl text-left'>Gallery</h1>
                 </div>
                 <PhotoAlbum
-                    layout="rows"
+                    layout="columns"
                     photos={photos}
                     targetRowHeight={150}
                     onClick={({ index }) => setIndex(index)}
